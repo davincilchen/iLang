f13ba312a24aa68ad16ed622b80ddf0bd5959814
Yuyu <qscez2001@gmail.com>
2018-03-12 15:30:31 +0800

    add restaurant new and create actions and views

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,4 +3,25 @@ class LessonsController < ApplicationController
   def index
     @lessons = Lesson.all
   end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      flash[:notice] = "lesson was successfully created"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "lesson was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :content)
+  end
 end
