ac332acbc8b6eed0769fc39721ea83a13945d70d
Yuyu <qscez2001@gmail.com>
2018-03-12 15:38:40 +0800

    add restaurant destroy action

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -19,6 +19,13 @@ class LessonsController < ApplicationController
     end
   end
 
+  def destroy
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to root_path
+    flash[:alert] = "lesson was deleted"
+  end
+
   private
 
   def lesson_params
