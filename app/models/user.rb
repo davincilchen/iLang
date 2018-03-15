1b9831bfec5dc010f3707a221b1088a9a8efdcbb
eric07109 <eric07109@gmail.com>
2018-03-15 19:58:01 +0800

    T2 unique contraint on username and email and alert

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
