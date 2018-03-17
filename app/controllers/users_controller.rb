71fee47b74e3f7f58ebb262670c5eaee6bec8ed2
eric07109 <eric07109@gmail.com>
2018-03-17 15:53:32 +0800

    create user show page to host choose language to learn and to teach

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:update, :edit, :tweets, :followings, :followers, :likes]
 
-  def index
-    @users = User.all
-  end
+	def index
+	@users = User.all
+	end
 
-  def learning
-  end
+	def edit
+	end
 
-  def teaching
-  end
+	def learning
+	end
+
+	def teaching
+	end
+
+	private
+	def user_params
+	params.require(:user).permit(:name, :introduction, :avatar)
+	end
+
+	def set_user
+	@user = User.find(params[:id])
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :lessons, only: [:new, :create, :destroy] 
 
-  resources :users, only: [:index, :edit, :create, :destroy] do
+  resources :users, only: [:index, :edit, :create, :destroy, :show] do
     member do 
     	post :learning
     	post :teaching
