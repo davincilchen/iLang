5d496a06e88b857cfeca3c43d010588bba6a50f1
Yuyu <qscez2001@gmail.com>
2018-03-23 16:46:55 +0800

    remove previous sorting function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,10 @@
 
 $(document).on('turbolinks:load', function(){
   $("table[role='datatable']").each(function(){
-    $(this).DataTable({});
+    $(this).DataTable({
+      processing: true,
+      serverSide: true,
+      ajax: $(this).data('url')
+    });
   });
 })
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,12 +77,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :description, :avatar)
   end
 
-  def sort_column
-    Lesson.column_names.include?(params[:sort]) ? params[:sort] : "title"
-  end
-  
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
 
 end
