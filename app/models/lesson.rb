f9efdd0ac3e2a6028a5a6b3557fc6d4adefe07ca
Brian <finder.chen@gmail.com>
2018-03-26 17:47:42 +0800

    add generate random 10 chacs string as etherpad-lite ID

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,4 +3,15 @@ class Lesson < ApplicationRecord
   belongs_to :teacher, class_name: "User"
   belongs_to :student, class_name: "User"
   belongs_to :language
+ 
+  def generate_random_pad
+    self.padID = random_string
+  end
+
+  def random_string(length=10)
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    randomstring = ''
+    length.times { randomstring << chars[rand(chars.size)] }
+    randomstring
+  end
 end
