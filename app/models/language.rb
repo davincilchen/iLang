aa2d7a0d181964b24b2f47f912da45c3748ce86f
Yuyu <qscez2001@gmail.com>
2018-03-28 19:47:46 +0800

    according to the language params to make update option

--- app/models/language.rb
+++ app/models/language.rb
@@ -6,10 +6,6 @@ class Language < ApplicationRecord
   has_many :learning_users, through: :learnings, source: :user
 
   has_many :lessons, dependent: :destroy
-
-  has_and_belongs_to_many :users
-
-  has_many :lessons, dependent: :destroy
   has_many :lessoning_users, through: :lessons, source: :user
 
 
