f1acb8b29192d3d639e0ff80cccf331bbee4d8b7
eric07109 <eric07109@gmail.com>
2018-03-28 23:06:18 +0800

    create uniqueness validateion in vocab model so that a student can have only an identical  key value instance once per language

--- app/models/learning.rb
+++ app/models/learning.rb
@@ -1,6 +1,8 @@
 class Learning < ApplicationRecord
 	validates :language_id, uniqueness: { scope: :user_id}
 
+	validates_uniqueness_of :student_id, scope: [:language_id, :key, :value]
+
 	belongs_to :user
 	belongs_to :language
 end
