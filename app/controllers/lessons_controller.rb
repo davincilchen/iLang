70d10b0b192a350c09e3530fac2c44864181d539
Yuyu <qscez2001@gmail.com>
2018-04-01 15:16:49 +0800

    fix deployment issue

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -82,8 +82,8 @@ class LessonsController < ApplicationController
   end
 
   def update
-    url = "http://127.0.0.1:9001/api/1/getText?apikey=60ab94c4ffb59abc33b3b5dcb2e92af48ac0b2544fa3d8ff3d7d172f573ee7c2&padID=#{@lesson.padID}"
-    # url = "http://ilang-etherpad-lite.herokuapp.com/api/1/getText?apikey=60ab94c4ffb59abc33b3b5dcb2e92af48ac0b2544fa3d8ff3d7d172f573ee7c2&padID=#{@lesson.padID}"
+    # url = "http://127.0.0.1:9001/api/1/getText?apikey=60ab94c4ffb59abc33b3b5dcb2e92af48ac0b2544fa3d8ff3d7d172f573ee7c2&padID=#{@lesson.padID}"
+    url = "http://ilang-etherpad-lite.herokuapp.com/api/1/getText?apikey=60ab94c4ffb59abc33b3b5dcb2e92af48ac0b2544fa3d8ff3d7d172f573ee7c2&padID=#{@lesson.padID}"
     response = RestClient.get(url)
     data = JSON.parse(response.body)
     @lesson.status = "false"
