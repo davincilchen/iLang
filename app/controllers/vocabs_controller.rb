af169f04a16af5b1752824561c1f09b17a372157
Brian <finder.chen@gmail.com>
2018-04-08 00:20:35 +0800

    add search vocabs by language or lesson

--- app/controllers/vocabs_controller.rb
+++ app/controllers/vocabs_controller.rb
@@ -1,5 +1,25 @@
 class VocabsController < ApplicationController
-  def index
-		@vocabs = current_user.vocabs
+	def index
+		@vocabs = nil
 	end 
+
+  before_action :get_lesson, only: [:show]
+  def show 
+    @vocabs = @lesson.vocabs
+  end
+  def search_vocabs
+    @vocabs = Vocab.where("language_id = ?", params[:language_id])    
+    if request.xhr?
+      respond_to do |format|
+        format.json {
+          render json: {vocabs: @vocabs}
+        }
+      end
+    end
+  end
+
+  private
+  def get_lesson
+    @lesson = Lesson.find(params[:id])
+  end
 end
