145420f1267696a24a5a559e4df3d872f60f5195
Brian <finder.chen@gmail.com>
2018-04-07 21:32:13 +0800

    set default vocabs is nil and add ajax method search_vocabs

--- app/controllers/vocabs_controller.rb
+++ app/controllers/vocabs_controller.rb
@@ -1,5 +1,16 @@
 class VocabsController < ApplicationController
-  def index
-		@vocabs = current_user.vocabs
+	def index
+		@vocabs = nil
 	end 
+
+  def search_vocabs
+    @vocabs = Vocab.where("language_id = ?", params[:language_id])    
+    if request.xhr?
+      respond_to do |format|
+        format.json {
+          render json: {vocabs: @vocabs}
+        }
+      end
+    end
+  end
 end
